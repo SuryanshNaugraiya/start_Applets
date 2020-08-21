9178f65afabf2746d6935f7ecba3b2144653b8e2
SuryanshNaugraiya <suryansh.naugraiya_cs18@gla.ac.in>
2020-08-21 10:11:10 +0530

    call fillOval() method to create tyres of car

--- src/appletsdemo/MakingCar.java
+++ src/appletsdemo/MakingCar.java
@@ -15,7 +15,9 @@ public class MakingCar extends Applet {
     public void paint(Graphics g) {
         g.setColor(Color.red);
         g.fillRect(200,100,200,100);
-        g.setColor(Color.red);
-        g.fillRect(100,100,300,50);
+        g.setColor(Color.black);
+        g.fillRect(100,200,400,100);
+        g.fillOval(150,300,50,50);
+        g.fillOval(400,300,50,50);
     }
 }
