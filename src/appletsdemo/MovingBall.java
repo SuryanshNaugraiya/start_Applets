4f71ce421ab5d28f16434d8fa156d14f8beaca81
SuryanshNaugraiya <suryansh.naugraiya_cs18@gla.ac.in>
2020-08-21 10:32:10 +0530

    used try catch to handle the exception thrown by sleep() method

--- src/appletsdemo/MovingBall.java
+++ src/appletsdemo/MovingBall.java
@@ -19,6 +19,14 @@ public class MovingBall extends Applet implements Runnable{
 
     @Override
     public void run() {
-        Thread.sleep(100);
+
+        try {
+
+            Thread.sleep(100);
+        }
+        catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
     }
 }
