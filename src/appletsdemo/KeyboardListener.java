82868b8466c170c25a1dd699529cf5f31b240ce7
SuryanshNaugraiya <suryansh.naugraiya_cs18@gla.ac.in>
2020-08-21 13:55:53 +0530

    override start() and paint() method

--- src/appletsdemo/KeyboardListener.java
+++ src/appletsdemo/KeyboardListener.java
@@ -1,4 +1,20 @@
 package appletsdemo;
 
-public class KeyboardListener {
+import java.applet.Applet;
+import java.awt.*;
+
+public class KeyboardListener extends Applet {
+
+    int x = 100 , y = 100;
+
+    @Override
+    public void start() {
+        setSize(800,600);
+        setBackground(Color.yellow);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillOval(x, y ,50,50);
+    }
 }
